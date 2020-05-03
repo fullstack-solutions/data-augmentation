52372144f379640e33387384d5fbffb4a08f6e53
Ibraheem Azeem <48335413+fullstack-solutions@users.noreply.github.com>
2020-05-03 21:34:35 +0200

    Data augmentation python main file

---
+++ augment_img.py
@@ -0,0 +1,26 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import os
+from PIL import Image
+datagen = ImageDataGenerator(shear_range=0.2, zoom_range=0.2) 
+filename= './data/'
+classes = os.listdir(filename)
+for class_name in classes:
+    class_path=os.path.join(filename, class_name)
+    images_names = os.listdir(class_path)
+    for image_name in images_names:
+
+        image_file_dir = os.path.join(class_path, image_name)
+        new_image_name= os.path.splitext(image_name)[0]
+        im = Image.open(image_file_dir).resize((224, 224))
+
+        x = img_to_array(im)
+        # Reshape the input image 
+        x = x.reshape((1, ) + x.shape)  
+        i = 0
+
+        # generate 5 new augmented images 
+        for batch in datagen.flow(x, batch_size = 1, 
+                        save_to_dir ='newdata'+'/'+class_name,save_prefix =new_image_name+str(i), save_format ='JPG'):
+            i += 1
+            if i > 4: 
+                break
